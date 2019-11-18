21aa15fe08b20b76e428aff8184f94ff31cf49ef
burntcustard <burntcustard@gmail.com>
2019-11-18 12:11:16 +0000

    Added untested defer-able sans-load-event-listener version

---
+++ scrollbar-width-defer.js
@@ -0,0 +1,49 @@
+function getScrollbarWidth() {
+    var outer = document.createElement('div');
+    outer.style.visibility = 'hidden';
+    outer.style.width = '100px';
+    document.body.append(outer);
+
+    var widthNoScroll = outer.offsetWidth;
+
+    // Force scrollbars
+    outer.style.overflow = 'scroll';
+
+    // Add innerdiv
+    var inner = document.createElement('div');
+    inner.style.width = '100%';
+    outer.append(inner);
+
+    var widthWithScroll = inner.offsetWidth;
+
+    // Remove divs
+    outer.remove();
+
+    return widthNoScroll - widthWithScroll;
+}
+
+function setScrollbarVar(value) {
+    document.body.style.setProperty('--scrollbar-width', value + 'px');
+}
+
+var config = {
+    threshold: 1.0
+}
+
+var handleIntersect = (entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.intersectionRatio < 1) {
+            setScrollbarVar(getScrollbarWidth());
+        } else {
+            setScrollbarVar(0);
+        }
+    });
+}
+
+var bodyScrollObserver = new IntersectionObserver(handleIntersect, config);
+
+bodyScrollObserver.observe(document.body);
+
+window.addEventListener('resize', () => {
+    setScrollbarVar(getScrollbarWidth());
+});
